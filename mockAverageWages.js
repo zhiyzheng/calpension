445396bd00258de6ef5f238d42b915705b0c3527
Jim IMAC YZ-020 <yuanzhi@Jims-MacBook-Pro.local>
2025-07-02 15:17:45 +0800

    Initial commit

---
+++ mockAverageWages.js
@@ -0,0 +1,7 @@
+window.mockAverageWages = [
+    { region: "全国", year2023: 120698, year2024: 124110 },
+    { region: "广东", year2023: 131418, year2024: 135395 },
+    { region: "上海", year2023: 191844, year2024: 197500 },
+    { region: "北京", year2023: 210000, year2024: 216300 }
+    // 可继续添加更多地区
+]; 
